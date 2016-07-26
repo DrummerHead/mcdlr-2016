ad558239247aa6ca337ad0db39b924bb5146e5b1
DrummerHead <drummerhead@gmail.com>
2016-07-26 16:02:11 -0400

    Add babelify and browserify to handle es6 js and imports

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,18 @@
 
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
+
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+
 const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
 const del = require('del');
 
-const reload = browserSync.reload;
 
 gulp.task('styles', () => {
   return gulp.src('source/stylesheets/main.scss')
@@ -31,7 +39,44 @@ gulp.task('sass-lint', () => {
     .pipe($.sassLint.failOnError())
 });
 
-gulp.task('serve', ['styles'], () => {
+const browserifyBabelify = watch => {
+  const rebundle = bundler => {
+    bundler.bundle()
+      .on('error', err => {
+        console.error(err);
+        this.emit('end');
+      })
+      .pipe(source('main.js'))
+      .pipe(buffer())
+      .pipe($.plumber())
+      .pipe($.sourcemaps.init({ loadMaps: true }))
+      .pipe($.sourcemaps.write('./'))
+      .pipe(gulp.dest('./.tmp/javascripts'))
+      .pipe(reload({ stream: true }));
+    console.log('-> done bundling!');
+  }
+
+  if (watch) {
+    var bundler = watchify(browserify('./source/javascripts/main.js', { debug: true }).transform(babelify));
+    rebundle(bundler);
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle(bundler);
+    });
+  }
+  else {
+    var bundler = browserify('./source/javascripts/main.js', { debug: true }).transform(babelify)
+    rebundle(bundler);
+  }
+}
+
+gulp.task('build-js', () => browserifyBabelify(false) );
+
+gulp.task('watch-js', () => browserifyBabelify(true) );
+
+gulp.task('clean', del.bind(null, ['.tmp', 'build']));
+
+gulp.task('serve', ['styles', 'watch-js'], () => {
   browserSync({
     notify: false,
     port: 9000,
@@ -41,5 +86,6 @@ gulp.task('serve', ['styles'], () => {
   gulp.watch('source/stylesheets/**/*.scss', ['styles']);
 });
 
-gulp.task('build', ['styles']);
+gulp.task('build', ['styles', 'build-js']);
+
 gulp.task('default', ['serve']);
