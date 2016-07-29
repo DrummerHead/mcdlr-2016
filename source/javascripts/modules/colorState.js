18da95e04c4738d12552777ac24e2a1815e21551
DrummerHead <drummerhead@gmail.com>
2016-07-28 23:22:37 -0400

    Apply eslint suggestions to colorState.js

--- source/javascripts/modules/colorState.js
+++ source/javascripts/modules/colorState.js
@@ -1,42 +1,46 @@
 const getRandomColor = () => {
-  return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+  const go = rand => Math.floor(rand * 256);
+  return `rgb(${go(Math.random())}, ${go(Math.random())}, ${go(Math.random())})`;
 };
 
-const getArticlesPositions = () => {
-  return Array.from(document.querySelectorAll('article'))
-    .map((article) => {
-      return {
-        id: article.id,
-        offset: article.offsetTop,
-        color: getRandomColor()
-      };
+const getArticlesPositions = () => Array.from(document.querySelectorAll('article'))
+  .map(article =>
+    ({
+      id: article.id,
+      offset: article.offsetTop,
+      color: getRandomColor(),
     })
-    .concat(
-      {
-        id: 'bung',
-        offset: document.body.scrollHeight,
-        color: getRandomColor()
-      }
-    )
-};
+  )
+  .concat({
+    id: 'bung',
+    offset: document.body.scrollHeight,
+    color: getRandomColor(),
+  });
+
+const isScrollingOnTheArticle = (i, articles, delta, windowOffset, lastArticle) =>
+  articles[i].offset - delta < windowOffset &&
+  windowOffset < articles[i + 1].offset - delta &&
+  lastArticle !== articles[i].id;
 
 const colorState = () => {
   const articles = getArticlesPositions();
-  const delta = Math.floor(window.innerHeight * (3/7));
+  const delta = Math.floor(window.innerHeight * (3 / 7));
   const body = document.body;
   let lastArticle;
 
-  for(const article of articles){
+  for (const article of articles) {
     const h2 = document.querySelector(`#${article.id} h2`);
-    h2 && h2.setAttribute('style', `background-color: ${article.color}`);
+    if (h2) {
+      h2.setAttribute('style', `background-color: ${article.color}`);
+    }
   }
 
-  window.addEventListener('scroll', (event) => {
+  window.addEventListener('scroll', () => {
     const windowOffset = window.scrollY;
-    for(const [i, article] of articles.entries()){
-      if(articles[i].offset - delta < windowOffset && windowOffset < articles[i+1].offset - delta && lastArticle !== article.id){
+    for (const [i, article] of articles.entries()) {
+      if (isScrollingOnTheArticle(i, articles, delta, windowOffset, lastArticle)) {
         lastArticle = article.id;
-        console.log('article.id: ' + article.id);
+        console.log(`article.id: ${article.id}`);
         body.setAttribute('style', `background-color: ${article.color}`);
         break;
       }
