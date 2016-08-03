a0d8bfdc9895aeff08a991807636e463bd1ca14e
DrummerHead <drummerhead@gmail.com>
2016-08-03 11:58:46 -0400

    eslint gulpfile, add comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
-'use strict';
-
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
 
-const source = require('vinyl-source-stream');
+const sourceStream = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const browserify = require('browserify');
 const watchify = require('watchify');
@@ -18,44 +16,59 @@ const del = require('del');
 const exec = require('child_process').exec;
 
 
-gulp.task('styles', () => {
-  return gulp.src('source/stylesheets/*.scss')
+// Style
+// =======================
+
+gulp.task('styles', () =>
+  gulp.src('source/stylesheets/*.scss')
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
     .pipe($.sass.sync({
       outputStyle: 'expanded',
       precision: 10,
-      includePaths: ['.']
+      includePaths: ['.'],
     }).on('error', $.sass.logError))
-    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
+    .pipe($.autoprefixer({ browsers: ['> 1%', 'last 2 versions', 'Firefox ESR'] }))
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest('.tmp/stylesheets'))
-    .pipe(reload({stream: true}));
-});
+    .pipe(reload({ stream: true }))
+);
 
-gulp.task('styles-minify', ['styles'], () => {
-  return gulp.src('.tmp/stylesheets/*.css')
-    .pipe($.cssnano({safe: true, autoprefixer: false}))
+
+// Style minification
+// -----------------------
+
+gulp.task('styles-minify', ['styles'], () =>
+  gulp.src('.tmp/stylesheets/*.css')
+    .pipe($.cssnano({ safe: true, autoprefixer: false }))
     .pipe(gulp.dest('.tmp/stylesheets/'))
-});
+);
 
-gulp.task('sass-lint', () => {
-  return gulp.src('source/stylesheets/*.scss')
+
+// Style linting
+// -----------------------
+
+gulp.task('sass-lint', () =>
+  gulp.src('source/stylesheets/*.scss')
     .pipe($.sassLint({
-      configFile: '.sass-lint.yml'
+      configFile: '.sass-lint.yml',
     }))
     .pipe($.sassLint.format())
     .pipe($.sassLint.failOnError())
-});
+);
 
-const browserifyBabelify = ({sourceFile = 'main', watch = true} = {}) => {
+
+// JavaScript
+// =======================
+
+const browserifyBabelify = ({ sourceFile = 'main', watch = true } = {}) => {
   const rebundle = bundler => {
     bundler.bundle()
       .on('error', err => {
         console.error(err);
         this.emit('end');
       })
-      .pipe(source(`${sourceFile}.js`))
+      .pipe(sourceStream(`${sourceFile}.js`))
       .pipe(buffer())
       .pipe($.plumber())
       .pipe($.sourcemaps.init({ loadMaps: true }))
@@ -63,34 +76,39 @@ const browserifyBabelify = ({sourceFile = 'main', watch = true} = {}) => {
       .pipe(gulp.dest('./.tmp/javascripts'))
       .pipe(reload({ stream: true }));
     console.log('-> done bundling!');
-  }
+  };
 
   if (watch) {
-    var bundler = watchify(browserify(`./source/javascripts/${sourceFile}.js`, { debug: true }).transform(babelify));
+    const bundler = watchify(
+      browserify(`./source/javascripts/${sourceFile}.js`, { debug: true })
+        .transform(babelify)
+    );
     rebundle(bundler);
     bundler.on('update', () => {
       console.log('-> bundling...');
       rebundle(bundler);
     });
-  }
-  else {
-    var bundler = browserify(`./source/javascripts/${sourceFile}.js`, { debug: true })
+  } else {
+    const bundler = browserify(`./source/javascripts/${sourceFile}.js`, { debug: true })
       .transform(rollupify)
       .transform(babelify)
       .transform(uglifyify);
 
     rebundle(bundler);
   }
-}
+};
 
-gulp.task('build-js', () => browserifyBabelify({watch: false}) );
+gulp.task('build-js', () => browserifyBabelify({ watch: false }));
 
-gulp.task('watch-js', () => browserifyBabelify() );
+gulp.task('watch-js', () => browserifyBabelify());
 
-gulp.task('build-js-color', () => browserifyBabelify({sourceFile: 'color-test', watch: false}) );
+gulp.task('build-js-color', () => browserifyBabelify({ sourceFile: 'color-test', watch: false }));
 
-gulp.task('watch-js-color', () => browserifyBabelify({sourceFile: 'color-test'}) );
+gulp.task('watch-js-color', () => browserifyBabelify({ sourceFile: 'color-test' }));
 
+
+// JavaScript minification
+// -----------------------
 
 gulp.task('rename-main-js', ['build-js'], cb => {
   exec('sleep 2 && mv .tmp/javascripts/{main,main-unminified}.js', (err, stdout, stderr) => {
@@ -100,12 +118,12 @@ gulp.task('rename-main-js', ['build-js'], cb => {
   });
 });
 
-gulp.task('scripts-minify', ['rename-main-js'], () => {
-  return gulp.src('.tmp/javascripts/main-unminified.js')
+gulp.task('scripts-minify', ['rename-main-js'], () =>
+  gulp.src('.tmp/javascripts/main-unminified.js')
     .pipe($.uglify())
     .pipe($.rename('main.js'))
-    .pipe(gulp.dest('.tmp/javascripts'));
-});
+    .pipe(gulp.dest('.tmp/javascripts'))
+);
 
 gulp.task('delete-extra-js', ['scripts-minify'], cb => {
   exec('rm .tmp/javascripts/main-unminified.js', (err, stdout, stderr) => {
@@ -115,32 +133,55 @@ gulp.task('delete-extra-js', ['scripts-minify'], cb => {
   });
 });
 
-gulp.task('js-lint', () => {
-  return gulp.src('source/javascripts/**/*.js')
-    .pipe(reload({stream: true, once: true}))
+
+// JavaScript linting
+// -----------------------
+
+const jsLint = source => () =>
+  gulp.src(source)
+    .pipe(reload({ stream: true, once: true }))
     .pipe($.eslint())
     .pipe($.eslint.format())
-    .pipe($.if(!browserSync.active, $.eslint.failAfterError()))
-});
+    .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
+
+gulp.task('js-lint', jsLint('source/javascripts/**/*.js'));
+
+gulp.task('gulpfile-lint', jsLint('gulpfile.js'));
+
+
+// Helper
+// =======================
 
 gulp.task('clean', del.bind(null, ['.tmp', 'build']));
+
+
+// Serving
+// =======================
 
 gulp.task('serve', ['styles', 'watch-js'], () => {
   browserSync({
     notify: false,
     port: 9000,
     proxy: 'http://localhost:4567/',
-    browser: 'google chrome'
+    browser: 'google chrome',
   });
 
   gulp.watch([
     'source/**/*.erb',
-    'data/*.yml'
+    'data/*.yml',
   ]).on('change', () => setTimeout(() => reload(), 1500));
 
   gulp.watch('source/stylesheets/**/*.scss', ['styles']);
 });
 
+
+// Building
+// =======================
+
 gulp.task('build', ['styles-minify', 'delete-extra-js']);
+
+
+// Default
+// =======================
 
 gulp.task('default', ['serve']);
