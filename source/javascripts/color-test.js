e1cd55fac098d8b5eefb1249928145333930da4b
DrummerHead <drummerhead@gmail.com>
2016-08-21 18:13:34 -0400

    Add possibility to test site colors on color-test

--- source/javascripts/color-test.js
+++ source/javascripts/color-test.js
@@ -2,31 +2,47 @@ import * as d3Color from 'd3-color';
 
 let allColors;
 
-for (const li of document.querySelectorAll('.color-test li')) {
-  const color = d3Color.cubehelix(li.dataset.colorSeed);
-  const newColor = color;
-  newColor.s = 1;
-  newColor.l = 0.95;
+// color.h is hue, from 0 to 360
+// color.s is saturation, from 0.0 to 2.0
+// color.l is lightness, from 0.0 to 1.0
+
+const colorTest = ({element, saturation, lightness}) => {
+  const color = d3Color.cubehelix(element.dataset.colorSeed);
+  let newColor = color;
+  newColor.s = saturation;
+  newColor.l = lightness;
 
   allColors +=
   `
-  id: ${li.id}
+  id: ${element.id}
   color:
-    seed: '${li.dataset.colorSeed}'
+    seed: '${element.dataset.colorSeed}'
     value: '${newColor.rgb().toString()}'
   `;
 
   const data = `<ul class='color-data'>
-    <li>id: ${li.id}</li>
-    <li>original: ${li.dataset.colorSeed}</li>
+    <li>id: ${element.id}</li>
+    <li>original: ${element.dataset.colorSeed}</li>
     <li>rgb: ${newColor.rgb()}</li>
     <li>h: ${newColor.h}</li>
-    <li>areEqual: ${newColor.rgb().toString() === li.dataset.colorValue}</li>
+    <li>areEqual: ${newColor.rgb().toString() === element.dataset.colorValue}</li>
   </ul>`;
 
-  li.insertAdjacentHTML('beforeend', data);
+  element.insertAdjacentHTML('beforeend', data);
 
-  li.setAttribute('style', `background-color: ${newColor.rgb()}`);
+  element.setAttribute('style', `background-color: ${newColor.rgb()}`);
 }
 
+for (const li of document.querySelectorAll('.site-colors')) {
+  colorTest({element: li, saturation: 1, lightness: 0.5});
+}
+
+for (const li of document.querySelectorAll('.article-colors')) {
+  colorTest({element: li, saturation: 1, lightness: 0.95});
+}
+
+document.querySelector('#link-color .color-data').setAttribute('style', `color: ${(() => {let c = d3Color.cubehelix("rgb(0,0,255)"); c.s = 1; c.l = .95; return c.rgb().toString()})()}`);
+
+// So from the console you `copy(allColors)`
 window.allColors = allColors;
+
