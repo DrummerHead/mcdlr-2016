0d19af78711a719c0f039bd791519ef1468493ab
DrummerHead <drummerhead@gmail.com>
2016-09-06 20:56:45 -0400

    Animate header only on first load

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,7 +1,23 @@
 import ColorState from './modules/colorState';
+// import showWidthInEms from './modules/showWidthInEms';
+
+// showWidthInEms(window);
+
 
 window.addEventListener('load', () => {
-  console.log('it loaded');
-  const colorState = new ColorState({ deltaRatio: 3 / 7, window, document });
+  const colorState = new ColorState({ deltaRatio: 2 / 5, window, document });
   colorState.init();
+
+
+  const header = document.querySelector("[itemtype='https://schema.org/WPHeader']");
+
+  setTimeout(() => {
+    header.classList.add('welcome-animation');
+  }, 500);
+
+  header.querySelector('svg').addEventListener('animationend', () => {
+    colorState.updateLayoutData();
+    header.classList.add('endstate-animation');
+    header.classList.remove('welcome-animation');
+  });
 });
